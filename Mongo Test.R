dc42c58d9c0e552fadfaac7afc7c06da6ba0b3a1
Valentin <53994707+vallivh@users.noreply.github.com>
2019-08-11 13:05:58 +0200

    Add files via upload

---
+++ Mongo Test.R
@@ -0,0 +1,25 @@
+library(mongolite)
+library(jsonlite)
+library(nycflights13)
+library(ndjson)
+
+m <- mongo(url = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", collection = "music", db = "mongotest")
+
+getwd()
+
+df <- stream_in("/Users/Valentin/Desktop/reviews_Musical_Instruments_5.json")
+typeof(df)
+m$insert(df)
+
+m$count()
+qry1 <- m$find('{"month":{ "$in": [1,2] }, "day":1, "carrier":"AA"}', fields = '{"year": true, "month": true, "day": true, "carrier": true}')
+qry2 <- m$distinct("month", query = '{"month":{"$in":[1,2]}}')
+qry3 <- m$aggregate('[
+          {"$match":{"carrier":{"$in":[""]}}},
+          {"$group":{"_id":"$carrier", "average_delay":{"$avg":"$arr_delay"}}}
+          ]')
+
+x <- m$find()
+typeof(x)
+str(x)
+x[1]
