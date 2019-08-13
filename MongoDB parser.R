a78a44d8d3e93e8edd9a4e85f50db48726bfca50
Valentin von Hammerstein <vonhammersteinv@gmail.com>
2019-08-13 11:53:00 +0200

    WIP

---
+++ MongoDB parser.R
@@ -0,0 +1,5 @@
+parseFields <- function(flist){
+  inBrackets <- paste0(paste0('"', flist, '": true'), collapse = ",")
+  fullQuery <- paste0("'{", inBrackets, "}'")
+  return(fullQuery) 
+}
--- Select Data Module.R
+++ Select Data Module.R
@@ -1,39 +1,44 @@
 library(shiny)
 library(mongolite)
 library(jsonlite)
-library(tidytext)
 library(ndjson)
-x <- library(janeaustenr)
+source("MongoDB parser.R")
 
 m <- mongo(url = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", db = "mongotest")
 
-m$count()
 collections <- m$run('{"listCollections":1}')
 df_runtime = data.frame()
 
 ui <- fluidPage(
   selectInput(inputId = "selectedColl", label = "Please select a dataset", choices = collections$cursor$firstBatch$name),
-  uiOutput("selectYear"),
+  uiOutput("selectVar"),
   actionButton("load", "Load Data"),
   tableOutput("table")
 )
 
 server <- function(input, output) {
   
-  dataset <- reactive({
-    ds <- mongo(url = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", collection = input$selectedColl, db = "mongotest")
-    ds$find(query = '{}', limit = 100)
+  db <- reactive({
+    mongo(url = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", collection = input$selectedColl, db = "mongotest")
   })
   
-  output$selectYear <- renderUI({
-    checkboxGroupInput("varChoice", "Please select variables", choices = colnames(dataset()), selected = colnames(dataset()), inline = TRUE)
+  ds <- reactive({
+    db()$find(query = '{}', limit = 50)
+  })  
+  
+  output$selectVar <- renderUI({
+    checkboxGroupInput("varSelected", "Please select variables", choices = colnames(ds()), 
+                       selected = colnames(ds()), inline = TRUE)
   })
   
-  output$table <- renderTable(dataset()[c(input$varChoice)])
+  output$table <- renderTable(ds()[c(input$varSelected)])
   
   observeEvent(input$load, {
-    df_runtime = dataset()
-    print(str(dataset()))
+    df_runtime = db()$find(query = '{}', fields = parseFields(input$varSelected))
+    insertUI(selector = "#load", where = "afterEnd", 
+             ui = textInput("success", label = NULL, width = '30%',
+                            value = "The dataset has been loaded to RAM and is available for all modules."))
+    print(nrow(df_runtime), ncol(df_runtime))
   })
 }
 
