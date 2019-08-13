b399664ff3d47019f47247e7d48f1729827fe649
Valentin von Hammerstein <vonhammersteinv@gmail.com>
2019-08-13 15:02:42 +0200

    added some comments for understandability

--- Select Data Module.R
+++ Select Data Module.R
@@ -4,9 +4,11 @@ library(jsonlite)
 library(ndjson)
 source("MongoDB parser.R")
 
+#connect to mongoDB and return all collections
 m <- mongo(url = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", db = "mongotest")
-
 collections <- m$run('{"listCollections":1}')
+
+#create global data frame for use in other modules (not working?)
 df_runtime = data.frame()
 
 ui <- fluidPage(
@@ -18,10 +20,12 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
+  #connect to collection selected in dropdown
   db <- reactive({
     mongo(url = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb", collection = input$selectedColl, db = "mongotest")
   })
   
+  #return some data for visual overview and to fetch variable names to select
   ds <- reactive({
     db()$find(query = '{}', limit = 50)
   })  
@@ -33,11 +37,13 @@ server <- function(input, output) {
   
   output$table <- renderTable(ds()[c(input$varSelected)])
   
+  #when Load Button is clicked, save selected data to global data frame
+  #data limited atm to 10.000 observations for speed!s
   observeEvent(input$load, {
     insertUI(selector = "#load", where = "afterEnd", 
              ui = textInput("success", label = NULL, width = '30%',
                             value = "The dataset has been loaded to RAM and is available for all modules."))
-    df_runtime = db()$find(query = '{}', fields = parseFields(input$varSelected))
+    df_runtime = db()$find(query = '{}', fields = parseFields(input$varSelected), limit = 10000)
   })
 }
 
